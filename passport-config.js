5c15b816fbcd0499f6c0f0a43894ace391678feb
memoldrem <madeline.moldrem@gmail.com>
2024-11-11 20:00:37 -0500

    better server setup

---
+++ passport-config.js
@@ -0,0 +1,8 @@
+const localStrategy = require('passport-local').Strategy
+
+function initializePassport(passport){
+    const authenticateUser = (email, password, done) => { // needs to interact with user model
+
+    }
+
+}
--- server.js
+++ server.js
@@ -2,13 +2,40 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-// var indexRouter = require('./routes/');
-var authRouter = require('./passport');
+const initializePassport = require('./passport-config')
+initializePassport(passport)
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
 
-app.use('/', authRouter);
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // add to database!!!
+    res.redirect('/')
+  } catch {
+    res.redirect('/register')
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
