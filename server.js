db2ddee5b2048a24c0e66159be7cad7cbae216f0
memoldrem <madeline.moldrem@gmail.com>
2024-11-11 20:31:20 -0500

    passport integration, still need to figure out database

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ const app = express();
 const port = 3000;
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+require('dotenv').config(); // if statement for production mode?
 
 const initializePassport = require('./passport-config')
-initializePassport(passport)
+initializePassport(passport, //*user email, user id*
+  // find user thru email in database!!!! need arg
+  // also pass in user id
+  )
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}))
+app.use(flash)
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
   res.render('index.ejs')
 })
 
@@ -19,9 +33,12 @@ app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate({
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
 
-})
+}))
 
 app.get('/register', (req, res) => {
   res.render('register.ejs')
@@ -36,6 +53,14 @@ app.post('/register', async (req, res) => {
     res.redirect('/register')
   }
 })
+
+
+function checkAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
